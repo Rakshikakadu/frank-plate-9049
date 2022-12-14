3051f5389564a6fdd749e2cc7dff060042ef2301
Tarique <anwartarique408@gmail.com>
2022-12-14 13:59:02 +0530

    Added project structure

---
+++ CabWay/src/test/java/com/cabway/CabWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
